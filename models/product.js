96312bc1fb5115654199543970be915bc86032d5
aniyy117 <janiket112@gmail.com>
2022-05-31 00:26:21 +0530

    changes in file

--- models/product.js
+++ models/product.js
@@ -1,17 +1,43 @@
- const products = [];
+import fs from "fs";
+import path from "path";
+import express from "express";
+
+const __dirname = path.resolve();
+
+const p = path.join(__dirname, "data", "products.json");
 
 export class Product {
-    constructor(title , price) {
-        this.title = title;
-        this.price = price;
-    }
+  constructor(title, price) {
+    this.title = title;
+    this.price = price;
+  }
 
- save =( ) =>{
-        console.log('Saving product');
-        products.push(this)
-    }
+  save = () => {
+    console.log("Saving product");
+    // products.push(this);
 
-    static fetchAll = () => {
-        return products;
-    }
+    fs.readFile(p, (err, data) => {
+      let products = [];
+
+      if (!err) {
+        products = JSON.parse(data);
+      }
+
+      products.push(this);
+
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        console.log(err);
+      });
+    });
+  };
+
+  static fetchAll = (cb) => {
+    fs.readFile(p, (err, data) => {
+      if (!err) {
+        cb(JSON.parse(data));
+      } else {
+        cb([]);
+      }
+    });
+  };
 }
