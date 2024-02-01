c34786f1b14f4980ba130413be5f271cfe1ffbf1
aniyy117 <janiket112@gmail.com>
2024-02-01 20:14:04 +0530

    learned basic of mongo

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
 const mongoConnect = require('./util/database').mongoConnect;
-
 const User = require('./models/user');
 
 const app = express();
@@ -22,11 +21,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
   User.findById("65bb801d075fa7ab13eb4292")
     .then(user => {
-      req.user = user;
+      req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
     .catch(err => console.log(err));
-  });
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
